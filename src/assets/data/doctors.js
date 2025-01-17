7d3a242b3c342d4778f595516a24cd53afc7d36d
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-01-18 01:44:58 +0600

    initialize the startup project of doctor appointment

---
+++ src/assets/data/doctors.js
@@ -0,0 +1,36 @@
+import doctorImg01 from "../images/doctor-img01.png";
+import doctorImg02 from "../images/doctor-img02.png";
+import doctorImg03 from "../images/doctor-img03.png";
+
+export const doctors = [
+  {
+    id: "01",
+    name: "Dr. Ayesha Rahman",
+    specialty: "Cardiologist",
+    avgRating: 4.9,
+    totalRating: 315,
+    photo: doctorImg01,
+    totalPatients: 1800,
+    hospital: "Apollo Hospital, Dhaka.",
+  },
+  {
+    id: "02",
+    name: "Dr. Imran Hossain",
+    specialty: "Orthopedic Surgeon",
+    avgRating: 4.7,
+    totalRating: 298,
+    photo: doctorImg02,
+    totalPatients: 1650,
+    hospital: "Square Hospital, Dhaka.",
+  },
+  {
+    id: "03",
+    name: "Dr. Nazmul Kabir",
+    specialty: "Pediatrician",
+    avgRating: 4.6,
+    totalRating: 250,
+    photo: doctorImg03,
+    totalPatients: 1400,
+    hospital: "United Hospital, Dhaka.",
+  },
+];
